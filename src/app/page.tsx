afa6d18d4c36a04c0ecf3510b6cb3dba8b65ca2e
水村駿吾 <mizumurashungo@mizumurashunwarenoMacBook-Pro.local>
2024-12-15 13:47:11 +0900

    pokeApiからデータを取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,20 @@
+"use client"
+
+import { useEffect } from "react";
+import {getAllPokemon} from "./utils/pokemon.js";
+
 export default function Home() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      const res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+
   return (
     <div>hello world</div>
   );
